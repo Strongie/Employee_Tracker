518466c0e73cd260c2000280f3af54d85e135a48
Strongie <nickstrong665@gmail.com>
2022-12-04 14:03:25 +1100

    set up schema sql

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
+CREATE TABLE departements (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30) NOT NULL,
+ 
+);
+
+CREATE TABle roles (
+
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT NOT NULL
+FOREIGN KEY (department_id)
+REFERENCES departments(id)
+ON DELETE SET NULL
+);
+
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL
+manager_id INT NOT NULL
+FOREIGN KEY (role_id)
+REFERENCES roles(id)
+ON DELETE SET NULL
+);
+
