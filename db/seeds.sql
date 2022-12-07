74d2f806f4e745d9a760913ad85c9b9306d84a9a
Strongie <nickstrong665@gmail.com>
2022-12-07 21:13:21 +1100

    set up seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES ('Engineer'),
+    ('Office'),
+    ('Sales'),
+    ('Intern');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Computer Engineer', 90000, 1),
+    ('Office Administrator', 60000, 2),
+    ('Salesperson', 75000, 3),
+    ('Intern', 40000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, 4),
+    ('Office Administrator', 2, NULL),
+    ('Salesperson', 2, 4),
+    ('Intern', 4, NULL);
