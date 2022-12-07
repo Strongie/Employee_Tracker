fb53e14585fb6baa345e926a2240db73e9b4f2f2
Strongie <nickstrong665@gmail.com>
2022-12-07 20:44:52 +1100

    setting up the inquirer prompts

--- server.js
+++ server.js
@@ -23,6 +23,75 @@ const db = mysql2.createConnection(
   console.log(`Connected to the employee_tracker database.`)
 );
 
+function init(){
+    employee_questions();
+}
+
+    function employee_questions(){
+        inquirer.prompt([
+          {
+            type: "list",
+            name: 'prompt',
+            message: 'what would you like to do?',
+            choices: ['view all departments', 'add a new department', 'view all roles', 'add a new role', 'view all employees', 'edit a current employee', 'view employees by their manager', 'view an employee by department']
+          }
+      ])
+      .then((data) => {
+        switch(data.choice){
+          case 'view all departments':
+            view_departments();
+          break;
+          case 'add a new department':
+            new_departments() 
+          break;
+          case 'view all roles':
+            view_roles();
+          break;
+          case 'add a new role':
+            new_role();
+          break;
+          case 'view all employees':
+            manager()
+          break;
+          case 'edit a current employee':
+            engineer()  
+          break;
+          case 'view employees by their manager':
+            intern()
+          break;
+          case 'view an employee by department':
+          default:
+            renderHTML()
+            console.log("Choice Made");
+    
+        }
+      });
+        
+function view_departments(){
+    db.query('SELECT * FROM department', (err, result)=>{
+        if(err) throw err;
+    return(result);
+    employee_questions();    
+
+});
+
+function new_departments(){
+    db.query('SELECT * FROM department', (err, result)=>{
+        if(err) throw err;
+    return(result);
+    employee_questions();    
+});
+
+function view_roles(){
+    db.query('SELECT * FROM department', (err, result)=>{
+        if(err) throw err;
+    return(result);
+    employee_questions();    
+
+});
 
 
+}
 
+}
+init()
